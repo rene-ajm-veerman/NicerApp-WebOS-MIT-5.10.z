b16eb04ea5a12ee496386ee257f282c4e0099b85
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2026-04-16 12:34:13 +0200

    #siteBackgrounds now uses 300xHeight thumbnails and lazy loading

--- NicerAppWebOS/businessLogic/vividUserInterface/v6.y.z/2D/background.js
+++ NicerAppWebOS/businessLogic/vividUserInterface/v6.y.z/2D/background.js
@@ -255,6 +255,7 @@ na.backgrounds = na.background = na.bg = {
             img.decoding = 'async';
             img.onclick = () => {
                 na.backgrounds.next("#siteBackground", null, '/siteMedia/backgrounds/Landscape/' + rfp);
+                $('#siteBackgrounds').fadeOut('normal');
             };
             img.alt = rfp;
             img.style.cssText = 'width:23%; cursor: pointer; height:auto; display:inline-block; margin:0.4em;';
